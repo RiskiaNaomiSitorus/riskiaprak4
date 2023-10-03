ad8254e31014d43891b40b8cc008460c4e5d0958
RiskiaNaomiSitorus <144434380+RiskiaNaomiSitorus@users.noreply.github.com>
2023-10-03 11:15:49 +0700

    Update script.js

--- pabwe-11s21046-praktikum4c/assets-4c/script.js
+++ pabwe-11s21046-praktikum4c/assets-4c/script.js
@@ -50,7 +50,7 @@ btnRoll.addEventListener("click", function () {
 
     // 2. Display dice
     diceEl.classList.remove("hidden");
-    diceEl.src = `/pabwe-11s21046-praktikum4c/assets-4c/dice-${dice}.png`;
+    diceEl.src = `/dice-${dice}.png`;
 
     // 3. Check for rolled 1: if true, switch to next player
     if (dice !== 1) {
